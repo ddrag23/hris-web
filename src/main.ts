7d58f95932d1036520df1f86702f105a26b64fa7
ddrag23 <kurodyami@gmail.com>
2023-12-11 22:01:16 +0700

    :hammer: add: reset store in pinia

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,15 @@ import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from './router'
 import { OhVueIcon, addIcons } from 'oh-vue-icons'
-import icons from '@/utils/icon'
+import icons from '@/plugins/icon'
+import { resetStore } from './utils'
 
 addIcons(...icons)
 const app = createApp(App)
 
-app.use(createPinia())
+const pinia = createPinia()
+pinia.use(resetStore)
+app.use(pinia)
 app.use(router)
 app.use(ElementPlus)
 app.component('VIcon', OhVueIcon)
